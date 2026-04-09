5d0bc74ddcfa7eb4cd08d34c2b093b8c038fdce8
Sumit Vishwkarma <justmultiplythinks@gmail.com>
2026-04-09 22:39:19 +0530

    typeape 0.1.3

--- Formula/typeape.rb
+++ Formula/typeape.rb
@@ -1,8 +1,8 @@
 class Typeape < Formula
   desc "Fast, minimal terminal typing speed test"
   homepage "https://github.com/realSUDO/typeape"
-  url "https://github.com/realSUDO/typeape/archive/refs/tags/v0.1.2.tar.gz"
-  sha256 "38bbf276856c8a71e407f45e84fa21be54e04847f0bcdd7cb26754420787ee8b"
+  url "https://github.com/realSUDO/typeape/archive/refs/tags/v0.1.3.tar.gz"
+  sha256 "dc71c3e4beba678a437c4c7881ddc054a05387d4512403be2f8dcf614714ca7f"
   license "GPL-3.0-or-later"
 
   depends_on "cmake" => :build
